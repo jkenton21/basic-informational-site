4a6f21437f9ac11bfadf1c70841090b6ee79e516
jkenton21 <jkenton21@gmail.com>
2022-08-23 15:16:20 -0500

    JS code updated to utilize express

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 //Index.js
 //NODE program for handling the basic informational website
 
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
-    if (err) throw err;
-    return data;
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
 });
 
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    let filename = "";
-    if (q.pathname === "/") {
-        filename = "." + "/index.html";
-    } else {
-        filename = "." + q.pathname;
-    }
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'))
+});
 
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.write(page404);
-            return res.end();
-        } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
-            return res.end();
-        }
-    });
-})
-.listen(8080);
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'))
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/404.html'))
+});
+
+app.listen(8080, () => {
+    console.log('Running app...')
+});
